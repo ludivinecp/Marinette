8159cc932bba350c5cea95c2aa1c7611aeec8451
ludivinecp <coipel.ludivine@gmail.com>
2015-10-16 11:13:06 +0200

    final project

--- app/controllers/localisations_controller.rb
+++ app/controllers/localisations_controller.rb
@@ -1,22 +1,21 @@
 class LocalisationsController < ApplicationController
 
 def index
-  @localisations = Localisation.last
   @localisation = Localisation.new
-  @forecast = ForecastIO.forecast(:latitude, :longitude)
-  
-
 end
 
 	def new
-		
+	@localisations = Localisation.last
+  @localisation = Localisation.new
+  @forecast = ForecastIO.forecast(:latitude, :longitude)
+
 	end
 
 
 def create
    @localisation = Localisation.new(localisation_params)
    if @localisation.save
-     redirect_to root_path
+     redirect_to new_localisation_path
    else
      render :new
    end
